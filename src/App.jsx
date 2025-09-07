90a8d8f7afc7b50ef825c1be182460d606502090
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2025-09-07 14:36:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Search from "./assets/components/search.jsx"
+
+const App = () => {
+    return(
+        <main>
+            <div className={"pattern"} />
+
+            <div className={"wrapper"}>
+                <header>
+                    <img src="./hero.png" alt="hero banner"/>
+                    <h1>Find <span className="text-gradient">Movies </span>You'll Enjoy Without the Hassle</h1>
+                </header>
+                <Search />
+            </div>
+
+
+        </main>
+
+
+    )
+
+}
+
+
+
+export default App
